9e19438f8d6445b9b6a979d2490f10905f375dc2
Muhammad Zeeshan <engr.zeeshan13@gmail.com>
2016-02-03 14:54:26 +0500

    first commit of project

---
+++ files-lister-deleter_GUI_CSharp/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace files_lister_deleter_GUI_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text_box_path = new System.Windows.Forms.TextBox();
+            this.text_box_extension = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.btn_run = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // text_box_path
+            // 
+            this.text_box_path.Location = new System.Drawing.Point(16, 48);
+            this.text_box_path.Name = "text_box_path";
+            this.text_box_path.Size = new System.Drawing.Size(309, 20);
+            this.text_box_path.TabIndex = 0;
+            // 
+            // text_box_extension
+            // 
+            this.text_box_extension.Location = new System.Drawing.Point(16, 117);
+            this.text_box_extension.Name = "text_box_extension";
+            this.text_box_extension.Size = new System.Drawing.Size(100, 20);
+            this.text_box_extension.TabIndex = 1;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(341, 29);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(298, 274);
+            this.checkedListBox1.TabIndex = 3;
+            // 
+            // btn_run
+            // 
+            this.btn_run.Location = new System.Drawing.Point(16, 173);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(75, 23);
+            this.btn_run.TabIndex = 2;
+            this.btn_run.Text = "Run";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.Click += new System.EventHandler(this.btn_run_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.AutoSize = true;
+            this.btn_delete.Location = new System.Drawing.Point(159, 173);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(93, 23);
+            this.btn_delete.TabIndex = 4;
+            this.btn_delete.Text = "Delete Selected";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Path to Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Extension of files to be Searched";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 361);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_run);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.text_box_extension);
+            this.Controls.Add(this.text_box_path);
+            this.Name = "Form1";
+            this.Text = "Files Lister Deleter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text_box_path;
+        private System.Windows.Forms.TextBox text_box_extension;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button btn_run;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ files-lister-deleter_GUI_CSharp/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace files_lister_deleter_GUI_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string path = "";
+        string ext = "";
+
+        private string[] replacePathString(string[] rootDir)
+        {
+            for (int i = 0; i < rootDir.Length; i++)
+            {
+                rootDir[i] = rootDir[i].Replace(path, "");
+            }
+            return rootDir;
+        }
+
+        /*  
+        * traceFiles(string,string)
+        * function responsible to detect files and then find for sub-directories
+        * if sub-directories are found, then its a recursive function
+        */
+
+        private void traceFiles(string rootDir, string extension)
+        {
+
+            string[] files;
+            string[] dirs;
+
+            files = Directory.GetFiles(@"" + rootDir, "*." + extension);
+
+            // replace the path
+            files = replacePathString(files);
+
+            // populate the checkedlistbox1
+            checkedListBox1.Items.AddRange(files);
+
+            // check for sub directories
+            dirs = Directory.GetDirectories(@"" + rootDir);
+
+            // if sub-directories are found, search for files in those too
+            if (dirs.Length != 0)
+            {
+                foreach (string d in dirs)
+                {
+                    traceFiles(d, extension);
+                }
+            }
+            return;
+        }
+
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            // clear the list if already exists
+            checkedListBox1.Items.Clear();
+
+            // get path provided by user in text box
+            path = text_box_path.Text;
+
+            // get extension provided by user in text box
+            ext = text_box_extension.Text;
+
+            // call the function to trace root folder for given extension of files
+            traceFiles(path, ext);
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            // delete each file selected by user
+            foreach (object itemChecked in checkedListBox1.CheckedItems)
+            {
+                File.Delete(path + (string)itemChecked);
+            }
+
+            // clear the list if already exists
+            checkedListBox1.Items.Clear();
+
+            // call the function to trace from the root folder again for given extension of files
+            traceFiles(path, ext);
+        }
+    }
+}
